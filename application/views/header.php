f4ddbf8021eeb27facea0d38fb31008076a765b7
dlim <dlim@vernier.com>
2017-02-11 10:12:31 -0800

    Initial commit for Mole Math app.

---
+++ application/views/header.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Mole Math</title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/foundation.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/app.css">
+</head>
+<body>
---
+++ application/views/question.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<header>
+		<h1>Mole Math</h1>
+	</header>
+</div>
+<div class="row">
+
+</div>
